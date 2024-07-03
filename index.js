fc2b4f1b2a0fc23a8d5e0557d56c19e0507bf9df
iftikhrlutfur <iftikherlutfur@gmail.com>
2024-07-04 02:43:04 +0600

    solving issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 //middleware
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5174','https://hotel-booking-service-250b8.web.app'],
   credentials: true
 }))
 app.use(express.json())
